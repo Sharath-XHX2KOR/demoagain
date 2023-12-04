b299cd666a6ddb6ec73809f70b07f45dd607fa52
sharath <s.sharath2@in.bosch.com>
2023-12-04 13:22:30 +0530

    deamon slay

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
 //set up project configuration in this file
 const cucumber = require('cypress-cucumber-preprocessor').default
-const report = require('mochawesome');
+const { marge } = require('mochawesome/reporter');
 const { defineConfig } = require("cypress");
 const fs = require('fs-extra');
 const path = require("path");
@@ -13,12 +13,23 @@ module.exports = defineConfig({
     video: true,
   e2e: {
     setupNodeEvents (on, config) {
-      on('after:run', () => {
-        report.generate({
-          jsonDir: 'cypress/reports/features',//path of json file
-          reportPath: 'cypress/reports'// path of reports
-        });
+      on('after:run', (results) => {
+        // Check if results include failures
+        const hasFailures = results.totalFailed > 0;
+
+        // Generate Mochawesome report
+        const options = {
+          reportTitle: 'Cypress Tests',
+          inline: true,
+          charts: true,
+          overwrite: false,
+          // other configuration options...
+        };
+
+        // Use marge to generate the report
+        marge(results, options);
       });
+
       on('file:preprocessor', cucumber())
       return fetchConfigurationByFile(config.env.configFile || "qa") || config;  //qa will be the default env if not mentioned.
     },
